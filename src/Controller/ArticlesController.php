46313e46e651df85962b8f625f581ff454d28a42
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-13 21:37:13 +0100

    create index

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,5 +4,10 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
-    
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
 }
