e08e3c61d71ccdd151b5eeb6fc843d99690c898c
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-02-13 21:54:49 +0100

    article list

---
+++ templates/Articles/index.php
@@ -0,0 +1,18 @@
+<h1> Articles </h1>
+<table>
+    <tr>
+        <th>Title</th>
+        <th>Created</th>
+    </tr>
+    <!-- iterate $articles query object, printing out article info-->
+    <?php foreach($articles as $article): ?>
+    <tr>
+        <td>
+            <?php $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+        </td>
+        <td>
+            <?php $article->created->format(DATE_RFC850) ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
